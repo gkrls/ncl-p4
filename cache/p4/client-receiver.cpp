5123fe5a929b1301437f782d9d1aa872c4fca66a
gkrls <gkrls.codes@gmail.com>
2024-08-20 01:31:08 +0200

    cache/p4 client4

--- cache/p4/client-receiver.cpp
+++ cache/p4/client-receiver.cpp
@@ -14,6 +14,9 @@
 #include <ostream>
 #include <random>
 #include <sys/socket.h>
+#include <sys/epoll.h>
+#include <fcntl.h>
+
 
 #include "client_utils.h"
 
@@ -151,13 +154,68 @@ ssize_t recv_all_udp_batch(int socket, void* buffer, size_t length, size_t packe
     return total_bytes_received;
 }
 
+void make_socket_non_blocking(int socket_fd) {
+    int flags = fcntl(socket_fd, F_GETFL, 0);
+    if (flags == -1) {
+        perror("fcntl F_GETFL");
+        exit(EXIT_FAILURE);
+    }
+    flags |= O_NONBLOCK;
+    if (fcntl(socket_fd, F_SETFL, flags) == -1) {
+        perror("fcntl F_SETFL");
+        exit(EXIT_FAILURE);
+    }
+}
+
+#define MAX_EVENTS 4
+
+void listen_to_sockets(int* sockets, size_t socket_count, size_t packet_size, size_t total_length, int batch_size) {
+    int epoll_fd = epoll_create1(0);
+    if (epoll_fd == -1) {
+        perror("epoll_create1");
+        exit(EXIT_FAILURE);
+    }
+
+    struct epoll_event event;
+    struct epoll_event events[MAX_EVENTS];
+
+    for (size_t i = 0; i < socket_count; ++i) {
+        make_socket_non_blocking(sockets[i]);
+
+        event.data.fd = sockets[i];
+        event.events = EPOLLIN;
+        if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, sockets[i], &event) == -1) {
+            perror("epoll_ctl");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    char* buffer = new char[total_length];
+
+    while (true) { // Run indefinitely or until you have received enough data
+        int n = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+        for (int i = 0; i < n; ++i) {
+            if (events[i].events & EPOLLIN) {
+                ssize_t bytes_received = recv_all_udp_batch(events[i].data.fd, buffer, total_length, packet_size, batch_size);
+                if (bytes_received < 0) {
+                    std::cerr << "Error receiving data on socket " << events[i].data.fd << std::endl;
+                } else {
+                    std::cout << "Thread " << std::this_thread::get_id() << " received " << bytes_received << " bytes on socket " << events[i].data.fd << std::endl;
+                }
+            }
+        }
+    }
+
+    delete[] buffer;
+    close(epoll_fd);
+}
 
 void receiver(uint32_t tid, std::string serverAddr, uint16_t serverPort,
               uint32_t keys, statistics &stats,
               std::shared_future<void> sigstart) {
   sigstart.wait();
-  log(tid) << "receiver: " << opt.IP << " - " << opt.Port + tid << '\n';
-  log(tid) << "  server: " << serverAddr << "-" << serverPort << '\n';
+  // log(tid) << "receiver: " << opt.IP << "-" << opt.Port + tid << '\n';
+  // log(tid) << "  server: " << serverAddr << "-" << serverPort << '\n';
 
   sockaddr_in server;
   server.sin_family = AF_INET;
@@ -169,31 +227,58 @@ void receiver(uint32_t tid, std::string serverAddr, uint16_t serverPort,
   addr.sin_addr.s_addr = inet_addr(opt.IP.c_str());
   addr.sin_port = htons(opt.Port + tid);
 
-  auto soc = socket(AF_INET, SOCK_DGRAM, 0);
-  if (soc < 0) {
-    std::cerr << "Socket creation failed: " << strerror(errno) << std::endl;
-    return;
-  }
 
-  int reuse = 1;
-  struct ifreq ifr;
-  memset(&ifr, 0, sizeof(ifr));
-  snprintf(ifr.ifr_name, sizeof(ifr.ifr_name), "ens4f0");
-  setsockopt(soc, SOL_SOCKET, SO_REUSEADDR, (void *)&reuse, sizeof(reuse));
-  setsockopt(soc, SOL_SOCKET, SO_REUSEPORT, (void *)&reuse, sizeof(reuse));
-  if (setsockopt(soc, SOL_SOCKET, SO_BINDTODEVICE, (void *)&ifr, sizeof(ifr)) < 0) {
-      std::cerr << "Binding to interface failed: " << strerror(errno) << std::endl;
-      return;
-  }
-  if (bind(soc, (sockaddr *)&addr, sizeof(sockaddr)) < 0) {
-    log(tid) << "error: bind socket to " << opt.IP << "." << opt.Port + tid
-             << '\n';
-    return;
-  }
 
-  sockaddr_in incaddrr;
-  socklen_t inclen = sizeof(sockaddr_in);
-  cache_h *q = (cache_h *) std::malloc((opt.Multiplier * keys) * sizeof(cache_h));
+  int sockets[4];
+
+  for (int i = 0; i < 4; ++i) {
+    sockets[i] = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockets[i] < 0) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(opt.Port + tid * 4 + i); // Unique port per socket
+
+    if (bind(sockets[i], (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("bind");
+        close(sockets[i]);
+        exit(EXIT_FAILURE);
+    }
+  }
+  auto tStart = std::chrono::high_resolution_clock::now();
+  // packet_size, total_length, batch_size
+  // opt.Multiplier * keys * CACHE_HEADER_SIZE - recvd, CACHE_HEADER_SIZE, 16
+  listen_to_sockets(sockets, 4, CACHE_HEADER_SIZE, opt.Multiplier * keys * CACHE_HEADER_SIZE, 16);
+  // auto soc = socket(AF_INET, SOCK_DGRAM, 0);
+  // if (soc < 0) {
+  //   std::cerr << "Socket creation failed: " << strerror(errno) << std::endl;
+  //   return;
+  // }
+
+  // int reuse = 1;
+  // struct ifreq ifr;
+  // memset(&ifr, 0, sizeof(ifr));
+  // snprintf(ifr.ifr_name, sizeof(ifr.ifr_name), "ens4f0");
+  // setsockopt(soc, SOL_SOCKET, SO_REUSEADDR, (void *)&reuse, sizeof(reuse));
+  // setsockopt(soc, SOL_SOCKET, SO_REUSEPORT, (void *)&reuse, sizeof(reuse));
+  // if (setsockopt(soc, SOL_SOCKET, SO_BINDTODEVICE, (void *)&ifr, sizeof(ifr)) < 0) {
+  //     std::cerr << "Binding to interface failed: " << strerror(errno) << std::endl;
+  //     return;
+  // }
+  // if (bind(soc, (sockaddr *)&addr, sizeof(sockaddr)) < 0) {
+  //   log(tid) << "error: bind socket to " << opt.IP << "." << opt.Port + tid
+  //            << '\n';
+  //   return;
+  // }
+
+  // sockaddr_in incaddrr;
+  // socklen_t inclen = sizeof(sockaddr_in);
+  // cache_h *q = (cache_h *) std::malloc((opt.Multiplier * keys) * sizeof(cache_h));
 
   // First packet indicates we not start receiving, start counting time
   // recvfrom(soc, q, CACHE_HEADER_SIZE, 0, (sockaddr *) &incaddrr, &inclen);
@@ -203,15 +288,15 @@ void receiver(uint32_t tid, std::string serverAddr, uint16_t serverPort,
   // }
   // auto tEnd = std::chrono::high_resolution_clock::now();
 
-  int recvd = recvfrom(soc, q, CACHE_HEADER_SIZE, 0, (sockaddr *) &incaddrr, &inclen);
-  auto tStart = std::chrono::high_resolution_clock::now();
-  recv_all_udp_batch(soc, q, opt.Multiplier * keys * CACHE_HEADER_SIZE - recvd, CACHE_HEADER_SIZE, 16);
+  // int recvd = recvfrom(soc, q, CACHE_HEADER_SIZE, 0, (sockaddr *) &incaddrr, &inclen);
+  // auto tStart = std::chrono::high_resolution_clock::now();
+  // recv_all_udp_batch(soc, q, opt.Multiplier * keys * CACHE_HEADER_SIZE - recvd, CACHE_HEADER_SIZE, 16);
   auto tEnd = std::chrono::high_resolution_clock::now();
   stats.duration =
       std::chrono::duration_cast<std::chrono::microseconds>(tEnd - tStart)
           .count();
   stats.queries = opt.Multiplier * keys;
-  close(soc);
+  // close(soc);
 }
 
 void loadKeys(const char *f, std::vector<uint64_t> &keys) {
